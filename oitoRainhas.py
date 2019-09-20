7259420e29cf95e41ee8d2b9f2bab3234cc21744
lucas csl <lcsl@cin.ufpe.br>
2019-09-20 17:02:12 -0300

    implementing calculation for individual fitness

--- oitoRainhas.py
+++ oitoRainhas.py
@@ -1,6 +1,10 @@
 from random import randint
 
+#base subject to create population without repetitions
 baseSubject = "000001010011100101110111"
+
+#solution for test purpose
+solution = "100000111011001110010101"
 
 def shuffle (amount, base):
     newSubject = base
@@ -19,11 +23,20 @@ def initialPopulation (size, baseIndividual):
         population[i] = shuffle(200, baseSubject)
     return population
 
-#def individualFitness (baseIndividual):
-#    colisions = 0
-#    for i in range(0, 6):
-#        value1 = ''.join(format(ord(n), 'b') for n in baseIndividual[(i*3):i+3])
+def individualFitness (baseIndividual):
+    colisions = 0
+    for i in range(0, 7):
+        ind = i*3
+        aux = baseIndividual[ind:(ind+3)]
+        value1 = int(bin(int(aux, 2)),2)
+        for e in range((i+1), 8):
+            ind2 = e * 3
+            aux2 = baseIndividual[ind2:(ind2 + 3)]
+            value2 = int(bin(int(aux2, 2)), 2)
+            distance = e - i
+            if ((value1 + distance) == value2) or ((value1 - distance) == value2):
+                colisions = colisions + 1
+    return(colisions)
 
-aux =
-v = ''.join(format(ord(n), 'b') for n in baseSubject[0:3])
-print(v)
+x = individualFitness(baseSubject)
+print x
